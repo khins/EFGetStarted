21d987e7ca1eeeadddf4fb5a3a4bcb64797d4b75
khins <kevin@hinshawfam.com>
2019-10-09 12:39:52 -0500

    Add project files.

---
+++ ConsoleApp1/Migrations/20191009160635_update.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateRecordWritten",
+                table: "Blogs",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateRecordWritten",
+                table: "Blogs");
+        }
+    }
+}
---
+++ ConsoleApp1/Migrations/20191009160908_UpdatePost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFGetStarted.Migrations
+{
+    public partial class UpdatePost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateRecordWritten",
+                table: "Posts",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateRecordWritten",
+                table: "Posts");
+        }
+    }
+}
